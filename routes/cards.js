7a15f0e85b7d6f09abf004068ec36503565edbbb
Sergei Vjuev <vjuev2013@yandex.ru>
2020-04-07 16:49:45 +0300

    fourth commit

--- routes/cards.js
+++ routes/cards.js
@@ -5,7 +5,14 @@ const fs = require('fs');
 
 let cards;
 fs.readFile(path.join(__dirname, '../data/cards.json'), { encoding: 'utf8' }, (err, data) => {
-  cards = JSON.parse(data);
+  if (!err) {
+    try {
+      cards = JSON.parse(data);
+    } catch (error) {
+      console.error(error);
+      cards = JSON.parse('[]');
+    }
+  }
 });
 router.get('/cards', (req, res) => {
   res.send(cards);
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable no-underscore-dangle */
 const router = require('express').Router();
@@ -6,15 +7,24 @@ const fs = require('fs');
 
 let users;
 fs.readFile(path.join(__dirname, '../data/users.json'), { encoding: 'utf8' }, (err, data) => {
-  users = JSON.parse(data);
+  if (!err) {
+    try {
+      users = JSON.parse(data);
+    } catch (error) {
+      console.error(error);
+      users = JSON.parse('[]');
+    }
+  }
 });
 router.get('/users', (req, res) => {
   res.send(users);
 });
 router.get('/users/:id', (req, res) => {
-  for (let index = 0; index < users.length; index += 1) {
-    if (users[index]._id === req.params.id) return res.send(users[index]);
+  const user = users.find((elem) => elem._id === req.params.id);
+
+  if (!user) {
+    return res.send(404, { message: 'Нет пользователя с таким id' });
   }
-  return res.send(404, { message: 'Нет пользователя с таким id' });
+  res.send(user);
 });
 module.exports = router;
